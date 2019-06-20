c4e56478dba008a081983a143902f5ba5f1df960
shin <proscus0706@gmail.com>
2019-06-20 09:33:15 +0900

    first_commit

---
+++ function-variable/cmake-build-debug/CMakeFiles/c_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_tutorial.dir/main.c.o"
+  "c_tutorial.pdb"
+  "c_tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ function-variable/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// function declaration
+int func();
+
+int main() {
+
+    // function call
+    int i = func();
+
+}
+
+// function definition
+int func(){
+    return 0;
+}
